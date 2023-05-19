fb39453f43ea7054360a9802864de7733ab51904
DevFC-M <felipech3344@gmail.com>
2023-05-19 20:19:50 -0300

    Criando a interface para receber o codigo, importação de fonts e implementação tailwindCSS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello World</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl font-title">
+        Hello World
+      </Text>
       <StatusBar style="light" />
     </View>
   );
